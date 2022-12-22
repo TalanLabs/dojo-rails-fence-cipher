4e9de2210ca1131b3f358b172a5018af2575845a
Guillaume AZAM <guillaumeazam@MacBook-Pro-de-TALAN.local>
2022-12-22 16:16:36 +0100

    feat(java): add java requirement

---
+++ java/src/main/java/RailFenceCipher.java
@@ -0,0 +1,16 @@
+
+public class RailFenceCipher {
+
+    public RailFenceCipher(final int rails) {
+    }
+  
+    String getEncryptedData(final String message) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    String getDecryptedData(final String message) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+  
+}
